373abcd8a6d7aeaf57b2b0a4492539b70d2038f7
Jeremy_P <jemyplu@gmail.com>
2020-05-05 16:17:54 +0200

    :art::sparkles: Details via dynamic routing works

---
+++ app/routes/rental.js
@@ -0,0 +1,27 @@
+import Route from '@ember/routing/route';
+
+const COMMUNITY_CATEGORIES = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export default class RentalRoute extends Route {
+  async model(params) {
+    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+    let { data } = await response.json();
+
+    return data.map(model => {
+      let { id, attributes } = model;
+      let type;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+        type = 'Community';
+      } else {
+        type = 'Standalone';
+      }
+
+      return { id, type, ...attributes };
+    });
+  }
+}

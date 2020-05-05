e16eddcf585320a030e74567cb7a03e0ad000ed4
Jeremy_P <jemyplu@gmail.com>
2020-05-05 15:50:33 +0200

    :white_check_mark: Rental test improved for dynamic routing

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -6,13 +6,15 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders information about a rental property', async function(assert) {
+  hooks.beforeEach(function() {
+    this.owner.setupRouter();
+  });
 
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders information about a rental property', async function(assert) {
 
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -32,6 +34,7 @@ module('Integration | Component | rental', function(hooks) {
 
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert.dom('article h3 a').hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
